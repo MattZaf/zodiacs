7d3a0747f4afc98b2cb2e8e0f314193c947bf76c
Matt Zafirovski <mattzaf@gmail.com>
2013-10-29 10:18:40 -0500

    updated bull twins crab

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ FortuneTeller::Application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
   # Uncomment each route below ONE AT A TIME and debug.
-  get("/zodiacs/aries",       { :controller => "zodiacs", action => "ram" })
-  # get("/zodiacs/taurus",      { :controller, "zodiacs", :action, "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs" :action => "crab" })
+  get("/zodiacs/aries",       { :controller => "zodiacs", :action => "ram" })
+  get("/zodiacs/taurus",      { :controller => "zodiacs", :action => "bull" })
+  get("/zodiacs/gemini",        { :controller => "zodiacs", :action => "twins" })
+  get("/zodiacs/cancer"       { :controller => "zodiacs", :action => "crab" })
   # get ("/zodiacs/leo",        { :controller => "zodiac", :action => "lion" })
-  # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
+  # get("/zodiacs/virgo",      { :controller => "zodiacs", :action => "maiden" })
   # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
   # get("/zodiac/scorpio",     { :controller => "zodiacs", :action => "scorpion" })
   # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
